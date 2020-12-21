4e82b70248434acddd84d762844f920a496936c0
Your Name <gpzlx1@mail.ustc.edu.cn>
2020-12-21 17:07:59 +0800

    filter dup \n and punctuation

--- DataHandling/TextHandling.py
+++ DataHandling/TextHandling.py
@@ -4,20 +4,12 @@ import os
 import multiprocessing
 from Cat_to_Chs import *
 
+WORK_NUM = 4
 
 target_dir_path = "/mnt/c/Users/gpzlx1/Desktop/netease/data/rock-songs"
 out_dir_parent = re.sub('data', 'preprocessed_data', target_dir_path)
-if not os.path.exists(out_dir_parent) or not os.path.isdir(out_dir_parent):
-    os.mkdir(out_dir_parent) 
-
-out_dir1 = out_dir_parent + "/finished/"
-out_dir2 = out_dir_parent + "/problem/"
-if os.path.exists(out_dir1) and os.path.isdir(out_dir1):
-    os.system("rm -r {}".format(out_dir1))
-os.mkdir(out_dir1)
-if  os.path.exists(out_dir2) and os.path.isdir(out_dir2):
-    os.system("rm -r {}".format(out_dir2))
-os.mkdir(out_dir2)
+os.system('rm -r {}'.format(out_dir_parent))
+os.system('mkdir {}'.format(out_dir_parent))
 
 
 def getlist(dir_path):
@@ -49,34 +41,18 @@ def text_handling(file_name):
     ## 文本过滤
     str1 = file.read()
     file.close()
-
+    #print(str1)
+    #print()
     str1 = re.sub(r'[(（].*[)）]', '', str1)
     str1 = re.sub(r'[^\n].*[:：].*\n', '', str1)
     str1 = re.sub(r'[^\n].*[-—]+.*\n', '', str1)
 
-    punc = '~`!#$%^&*()_+-=|\';":/.,?><~·！@#￥%……&*（）——+-=“：’；、。，？》《{}'
-    str1 = re.sub(r'[%s]+' % punc, ' ', str1)
+    str1 = re.sub(r'[^\u4e00-\u9fa5\nA-Za-z0-9 ]', ' ', str1)
     str1 = re.sub(r' +', ' ', str1)
+    str1 = re.sub(r'^(\s*)\n', '', str1)
+    str1 = re.sub(r'(\s*)\n$', '', str1)
+    str1 = re.sub(r'\n[\s\n]*\n', '\n', str1)
     return str1
-
-
-def title_handling(file_name, str1, out_dir1, out_dir2):
-    file_name = re.sub(r'.*/', '', file_name)
-    if re.search(r'[《].*[》]', file_name):
-        file_name = '{}{}'.format(out_dir2, file_name)
-        return file_name
-
-    result = re.search(r'\d+_[\u4e00-\u9fa5]+', file_name)
-    if result:
-        # file_name = re.sub(r'[(（【\[].*[)）】\]]', '', file_name)
-        # file_name = re.sub(r'[-—].*', '.txt', file_name)
-        work_dir = 'data/data2/'
-        file_name = '{}{}{}'.format(out_dir1, result.group(0), '.txt')
-    else:
-        work_dir = 'data/data3/'
-        file_name = '{}{}'.format(out_dir2, file_name)
- 
-    return file_name
 
 
 def chinese_or_not(str1):
@@ -95,7 +71,7 @@ def chinese_or_not(str1):
             chinesecount = chinesecount + 1
         count = count + 1
     # print(chinesecount / count)
-    if (chinesecount / count >= 0.6):
+    if (chinesecount / count >= 0.5):
         return True
     else:
         return False
@@ -106,7 +82,8 @@ def data_handling(file_name):
         str1 = text_handling(file_name)
         if chinese_or_not(str1):
             str1 = traditional2simplified(str1)
-            new_file_name = title_handling(file_name, str1, out_dir1, out_dir2)
+            file_name = file_name.split('/')[-1]
+            new_file_name = os.path.join(out_dir_parent, file_name)
             with open(new_file_name, mode='w') as f:
                 f.write(str1)
         else:
@@ -119,8 +96,9 @@ def data_handling(file_name):
 
 def main():
     song_list = getlist(target_dir_path)
-    with multiprocessing.Pool(12) as p:
+    with multiprocessing.Pool(WORK_NUM) as p:
         p.map(data_handling, song_list)
+
 
 
 
