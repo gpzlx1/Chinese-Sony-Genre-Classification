1ca60f006792743078b8a67c2f3966cfc3d91e31
hipress <hipressX@outlook.com>
2020-12-24 18:07:59 +0800

    rm some print

--- NN/train_eval.py
+++ NN/train_eval.py
@@ -6,7 +6,6 @@ import numpy as np
 def adjust_learning_rate(optimizer, epoch, lr):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     lr = lr * (0.1 ** (epoch // 10))
-    print(lr)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
         
